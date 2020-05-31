b63755eee754cadc4b0ebb6b2760a8e5b01b6e4f
IBR <business1232@gmail.com>
2020-05-31 21:28:50 +0200

    Deploy 1.1

--- peerServer.js
+++ peerServer.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { PeerServer } = require("peer");
 
 const peerServer = PeerServer({
-	port: 9000,
+	port: 80,
 	path: "/",
 	ssl: {
 		key: fs.readFileSync("sslcert/key.pem"),
@@ -10,4 +10,4 @@ const peerServer = PeerServer({
 	},
 });
 
-console.log("PeerServer is running on port 9000 with SSL");
+console.log("PeerServer is running on port 80 with SSL");
