78800d307dcc8810ac66f1db3b6d2016c6ecb349
IBR <business1232@gmail.com>
2020-06-04 08:19:43 +0200

    adding express and cors

--- peerServer.js
+++ peerServer.js
@@ -1,13 +1,28 @@
-const fs = require("fs");
-const { PeerServer } = require("peer");
+const fs = require("fs"),
+	privateKey = fs.readFileSync("sslcert/key.pem", "utf8"), // SSL cert
+	certificate = fs.readFileSync("sslcert/cert.pem", "utf8"),
+	credentials = { key: privateKey, cert: certificate }, // add to createServer and change http to https
+	https = require("https"),
+	express = require("express"),
+	app = express(),
+	httpsServer = https.createServer(credentials, app),
+	port = process.env.PORT || 433,
+	{ ExpressPeerServer } = require("peer"),
+	cors = require("cors");
 
-const peerServer = PeerServer({
-	port: process.env.PORT || 433,
+const corsOptions = {
+	origin: "https://ib-r.github.io/webRTC",
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+const peerServer = ExpressPeerServer(httpsServer, {
 	path: "/",
-	// ssl: {
-	// 	key: fs.readFileSync("sslcert/key.pem"),
-	// 	cert: fs.readFileSync("sslcert/cert.pem"),
-	// },
+	debug: true,
 });
 
-console.log("PeerServer is running on port 443 with SSL");
+app.use(cors(corsOptions))
+app.use("/", peerServer);
+
+httpsServer.listen(port);
+
+console.log(`Server is running on port ${port} with SSL`);
